688a85a403533615708c5d1ae7a7a06e3d60c783
AlexanderFengFeng <39276953+AlexanderFengFeng@users.noreply.github.com>
2022-12-26 23:24:36 -0800

    Initial commit

---
+++ Source/Phasmus/PhasmusGameMode.cpp
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PhasmusGameMode.h"
+#include "PhasmusCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+APhasmusGameMode::APhasmusGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+}
