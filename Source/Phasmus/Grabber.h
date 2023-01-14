c5bfa84d82ed5689a20496154d21e071a8efd610
AlexanderFengFeng <39276953+AlexanderFengFeng@users.noreply.github.com>
2023-01-13 23:43:16 -0800

    Create Grabber class to be used by FP player.

---
+++ Source/Phasmus/Grabber.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SphereComponent.h"
+#include "Grabber.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PHASMUS_API UGrabber : public USphereComponent
+{
+	GENERATED_BODY()
+	
+};
