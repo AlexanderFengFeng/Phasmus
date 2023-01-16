6ab6122b54f093c8cc22cb152b1e6a8f6dc3e74f
AlexanderFengFeng <39276953+AlexanderFengFeng@users.noreply.github.com>
2023-01-15 19:57:51 -0800

    Comment out code in PhasmusGameInstance.
    Might be used instead in PlayerController.

---
+++ Source/Phasmus/PhasmusGameInstance.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PhasmusGameInstance.h"
+
+#include "UI/HeadsUpDisplay.h"
+#include "UObject/ConstructorHelpers.h"
+
+
+//UPhasmusGameInstance::UPhasmusGameInstance()
+//{
+//    ConstructorHelpers::FClassFinder<UHeadsUpDisplay> HUDBPClass(TEXT("/Game/UI/WBP_HUD"));
+//    if (HUDBPClass.Class != nullptr)
+//    {
+//        HUD = HUDBPClass.Class;
+//    }
+//}
+
---
+++ Source/Phasmus/PhasmusGameInstance.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "PhasmusGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PHASMUS_API UPhasmusGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+//	TSubclassOf<class UHeadUpDisplay> HUD;
+//
+//public:
+//	UPhasmusGameInstance();
+};
