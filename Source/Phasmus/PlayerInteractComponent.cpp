4033bcb5aa276861ee183463276d66dbad06e5c7
AlexanderFengFeng <39276953+AlexanderFengFeng@users.noreply.github.com>
2023-01-17 22:48:09 -0800

    Add LineTraceSingleByChannel to PlayerInteractComponent.

--- Source/Phasmus/PlayerInteractComponent.cpp
+++ Source/Phasmus/PlayerInteractComponent.cpp
@@ -38,10 +38,20 @@ void UPlayerInteractComponent::LookForInteractable()
 		UE_LOG(LogTemp, Error, TEXT("Error: First-person camera component is required"));
 		return;
 	}
-	FHitResult HitResult;
-
 	FVector Start = FirstPersonCameraComponent->GetComponentLocation();
 	FVector ForwardVector = FirstPersonCameraComponent->GetForwardVector();
 	FVector End = Start + (ForwardVector * InteractDistance);
 	DrawDebugLine(GetWorld(), Start, End, FColor::Green, false, 1, 0, 1);
+
+	FHitResult HitResult;
+	FCollisionQueryParams CollisionParams;
+	if (GetWorld()->LineTraceSingleByChannel(HitResult, Start, End, ECC_Visibility, CollisionParams))
+	{
+	    if (GEngine)
+	    {
+			GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Red, FString::Printf(TEXT("You are hitting: %s"), *HitResult.GetActor()->GetName()));
+			GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Red, FString::Printf(TEXT("Impact Point: %s"), *HitResult.ImpactPoint.ToString()));
+			GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Red, FString::Printf(TEXT("Normal Point: %s"), *HitResult.ImpactNormal.ToString()));
+	    }
+	}
 }
