a2b3973e563b3ce6a094af9e79a98a47384c354d
AlexanderFengFeng <39276953+AlexanderFengFeng@users.noreply.github.com>
2023-01-17 22:17:38 -0800

    Batch push

---
+++ Source/Phasmus/Interactables/Pickup.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pickup.h"
+#include "Components/SphereComponent.h"
+
+// Sets default values
+APickup::APickup()
+{
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere Collider"));
+	SetRootComponent(SphereComponent);
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Static Mesh"));
+	StaticMesh->SetupAttachment(SphereComponent);
+
+	SphereComponent->OnComponentBeginOverlap.AddDynamic(this, &APickup::OnSphereBeginOverlap);
+	SphereComponent->OnComponentEndOverlap.AddDynamic(this, &APickup::OnSphereEndOverlap);
+}
+
+// Called when the game starts or when spawned
+void APickup::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APickup::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+/**
+ * Dynamic delegate called when a component overlaps with the sphere collider.
+ */
+void APickup::OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+
+}
+
+/**
+ * Dynamic delegate called when a component stops overlapping with the sphere collider.
+ */
+void APickup::OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+
+}
---
+++ Source/Phasmus/Interactables/Pickup.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Pickup.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class PHASMUS_API APickup : public AActor
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = Components)
+	USphereComponent* SphereComponent;
+    UPROPERTY(EditAnywhere, Category = Components)
+	UStaticMeshComponent* StaticMesh;
+
+	
+public:	
+	// Sets default values for this actor's properties
+	APickup();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	void OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+};
