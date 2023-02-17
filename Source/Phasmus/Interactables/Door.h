3807e2a4d9eb03b72fc9db00063f2627b6a0e921
AlexanderFengFeng <39276953+AlexanderFengFeng@users.noreply.github.com>
2023-02-16 22:35:18 -0800

    Create Door class.
    To be filled in later.

---
+++ Source/Phasmus/Interactables/Door.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Interactable.h"
+#include "Door.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PHASMUS_API ADoor : public AInteractable
+{
+	GENERATED_BODY()
+	
+};
