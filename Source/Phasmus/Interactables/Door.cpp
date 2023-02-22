7dbaf1f60a4456bb14cb1ca9e9397677db565e36
AlexanderFengFeng <39276953+AlexanderFengFeng@users.noreply.github.com>
2023-02-21 23:08:39 -0800

    Implement prototype for rotating door.

--- Source/Phasmus/Interactables/Door.cpp
+++ Source/Phasmus/Interactables/Door.cpp
@@ -3,3 +3,40 @@
 
 #include "Door.h"
 
+void ADoor::BeginPlay()
+{
+    Super::BeginPlay();
+    ClosedYaw = GetActorRotation().Yaw;
+    OnInteract.AddDynamic(this, &ADoor::OpenDoor);
+}
+
+
+void ADoor::OpenDoor(APhasmusPlayerCharacter* PlayerCharacter)
+{
+    UE_LOG(LogTemp, Warning, TEXT("ClosedYaw: %f"), ClosedYaw);
+    bIsRotating = true;
+    bIsInteractable = false;
+}
+
+void ADoor::Tick(float DeltaSeconds)
+{
+    if (bIsRotating)
+    {
+        float TargetYaw = GetActorRotation().Yaw + RotationSpeed * DeltaSeconds;
+        UE_LOG(LogTemp, Warning, TEXT("ClosedYaw: %f"), ClosedYaw);
+        UE_LOG(LogTemp, Warning, TEXT("Diff: %f"), FMath::FindDeltaAngleDegrees(TargetYaw, ClosedYaw));
+        if (FMath::FindDeltaAngleDegrees(TargetYaw, ClosedYaw) >= 90.f)
+        //if (FMath::Abs(FMath::FindDeltaAngleDegrees(TargetYaw, ClosedYaw)) >= 90.f)
+        {
+            TargetYaw = ClosedYaw + 90.f;
+            bIsRotating = false;
+            bIsInteractable = true;
+        }
+        //UE_LOG(LogTemp, Warning, TEXT("TargetYaw2: %f"), TargetYaw);
+        SetActorRotation(FRotator(GetActorRotation().Pitch, TargetYaw, GetActorRotation().Roll));
+        //AddActorLocalRotation(FRotator(0.f, TargetYaw, 0.f));
+
+        //FRotator NewRelativeRotation = (GetActorRotation())
+        //SetActorRelativeRotation()
+    }
+}
--- Source/Phasmus/Interactables/Door.h
+++ Source/Phasmus/Interactables/Door.h
@@ -13,5 +13,22 @@ UCLASS()
 class PHASMUS_API ADoor : public AInteractable
 {
 	GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere)
+    bool bOpensClockwise = false;
+	bool bIsClosed = true;
+	bool bIsRotating = false;
+
+	float ClosedYaw;
+
+	float RotationSpeed = 30.f;
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
+
+public:
+	UFUNCTION()
+	void OpenDoor(class APhasmusPlayerCharacter* PlayerCharacter);
 	
 };
