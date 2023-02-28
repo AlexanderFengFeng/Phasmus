7b7c8141ee6076f08b9649b132363a5bb6a05fee
AlexanderFengFeng <39276953+AlexanderFengFeng@users.noreply.github.com>
2023-02-27 19:13:40 -0800

    Implement behavior for PhasmoDoor.

--- Source/Phasmus/Interactables/PhasmoDoor.cpp
+++ Source/Phasmus/Interactables/PhasmoDoor.cpp
@@ -6,6 +6,11 @@
 void APhasmoDoor::BeginPlay()
 {
     Super::BeginPlay();
+    ClosedDegrees = GetActorRotation().Yaw;
+    InitialInFrontForwardVector = -GetActorForwardVector();
+    MinClampedDegrees = bOpensCounterClockwise ? ClosedDegrees : ClosedDegrees - OpenDegrees;
+    MaxClampedDegrees = bOpensCounterClockwise ? ClosedDegrees + OpenDegrees : ClosedDegrees;
+
     OnInteract.AddDynamic(this, &APhasmoDoor::GrabDoor);
     OnReleaseInteraction.AddDynamic(this, &APhasmoDoor::ReleaseDoor);
 }
@@ -13,15 +18,46 @@ void APhasmoDoor::BeginPlay()
 void APhasmoDoor::Tick(float DeltaSeconds)
 {
     Super::Tick(DeltaSeconds);
+    MoveDoorOnTick(DeltaSeconds);
 }
 
 void APhasmoDoor::GrabDoor(APhasmusPlayerCharacter* PlayerCharacter)
 {
-    
+    GrabbingCharacter = PlayerCharacter;
+    bIsGrabbed = true;
+    bIsInteractable = false;
 }
 
 void APhasmoDoor::ReleaseDoor(APhasmusPlayerCharacter* PlayerCharacter)
 {
-    
+    GrabbingCharacter = nullptr;
+    bIsGrabbed = false;
+    bIsInteractable = true;
+}
+
+void APhasmoDoor::MoveDoorOnTick(float DeltaSeconds)
+{
+    // TODO: Fix issue with door colliding/not colliding properly.
+    if (bIsGrabbed && GrabbingCharacter != nullptr)
+    {
+        // Base sign for horizontally moving the door on the forward vector of the closed Door.
+        int PlayerInFrontOfDoorHorizontally = FVector::DotProduct(
+            GrabbingCharacter->GetActorLocation() - GetActorLocation(),
+            InitialInFrontForwardVector
+        ) >= 0 ? 1 : -1;
+        // Base sign for vertically moving the door on the current forward vector of the Door.
+        int PlayerInFrontOfDoorVertically = FVector::DotProduct(
+            GrabbingCharacter->GetActorLocation() - GetActorLocation(),
+            -GetActorForwardVector()
+        ) >= 0 ? 1 : -1;
+
+        float OpenRateBasedOnHorizontal = GrabbingCharacter->GetLookRightRate() * (bOpensCounterClockwise ? 1 : -1) * PlayerInFrontOfDoorHorizontally;
+        // Looking up will open doors.
+        float OpenRateBasedOnVertical = -GrabbingCharacter->GetLookUpRate() * PlayerInFrontOfDoorVertically;
+        float OpenRate = OpenRateBasedOnHorizontal + OpenRateBasedOnVertical;
+
+        float NextYaw = FMath::ClampAngle(GetActorRotation().Yaw + OpenRate, MinClampedDegrees, MaxClampedDegrees);
+        SetActorRotation(FRotator(GetActorRotation().Pitch, NextYaw, GetActorRotation().Roll));
+    }
 }
 
--- Source/Phasmus/Interactables/PhasmoDoor.h
+++ Source/Phasmus/Interactables/PhasmoDoor.h
@@ -7,7 +7,6 @@
 #include "PhasmoDoor.generated.h"
 
 class APhasmusPlayerCharacter;
-
 /**
  * 
  */
@@ -16,10 +15,29 @@ class PHASMUS_API APhasmoDoor : public AInteractable
 {
 	GENERATED_BODY()
 
+
+	UPROPERTY(EditAnywhere)
+	bool bOpensCounterClockwise = true;
+
+	// TODO: Consider allowing doors to be placed in editor already open (not high priority).
+	float ClosedDegrees;
+
+	UPROPERTY(EditAnywhere, meta = (ClampMin = "0", ClampMax = "180", UIMin = "0", UIMax = "180"))
+	float OpenDegrees = 90.f;
+
+	float MinClampedDegrees;
+	float MaxClampedDegrees;
+
+	bool bIsGrabbed = false;
+
 	UFUNCTION()
 	void GrabDoor(APhasmusPlayerCharacter* PlayerCharacter);
 	UFUNCTION()
 	void ReleaseDoor(APhasmusPlayerCharacter* PlayerCharacter);
+
+	APhasmusPlayerCharacter* GrabbingCharacter;
+	FVector InitialInFrontForwardVector;
+	void MoveDoorOnTick(float DeltaSeconds);
 
 protected:
 	virtual void BeginPlay() override;
