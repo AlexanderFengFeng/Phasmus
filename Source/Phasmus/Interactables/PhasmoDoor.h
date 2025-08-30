e759b8932e8fa4bc3a51e291a32ebd111e4f9926
AlexanderFengFeng <39276953+AlexanderFengFeng@users.noreply.github.com>
2025-08-29 18:46:01 -0700

    [Editor] Jetbrains type sh

--- Source/Phasmus/Interactables/PhasmoDoor.h
+++ Source/Phasmus/Interactables/PhasmoDoor.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -7,6 +5,7 @@
 #include "PhasmoDoor.generated.h"
 
 class APhasmusPlayerCharacter;
+
 /**
  * 
  */
